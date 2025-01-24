67f47e1b48a34ae4a3f8ddb29bd5ab83a605e2a3
Andrii Savluk <a.savluk@ideil.com>
2025-01-24 17:21:40 +0200

    v2.21.0

--- src/Support/Mapping/Casts/IntersectionCast.php
+++ src/Support/Mapping/Casts/IntersectionCast.php
@@ -2,6 +2,8 @@
 
 namespace Savks\Negotiator\Support\Mapping\Casts;
 
+use LogicException;
+use Savks\Negotiator\Exceptions\UnexpectedValue;
 use Savks\Negotiator\Support\TypeGeneration\Types\Types;
 use stdClass;
 
@@ -14,17 +16,52 @@ class IntersectionCast extends Cast
         $this->casts = $objects;
     }
 
-    protected function finalize(mixed $source, array $sourcesTrace): stdClass
+    protected function finalize(mixed $source, array $sourcesTrace): stdClass|array
     {
-        $result = new stdClass();
+        $result = null;
+
+        $initialResultType = null;
 
         foreach ($this->casts as $cast) {
             $objectResult = $cast->resolve($source, $sourcesTrace);
 
-            if ($objectResult !== null) {
+            if ($objectResult === null) {
+                continue;
+            }
+
+            if (
+                ! is_array($objectResult)
+                && (
+                    ! is_object($objectResult)
+                    || ! ($objectResult instanceof stdClass)
+                )
+            ) {
+                throw new UnexpectedValue(['stdClass', 'array<string, mixed>'], $objectResult);
+            }
+
+            if (is_array($objectResult) && ! $objectResult) {
+                continue;
+            }
+
+            $currentResultType = is_object($objectResult) || ! array_is_list($objectResult) ? 'object' : 'list';
+
+            if (! $initialResultType) {
+                $initialResultType = $currentResultType;
+
+                $result = match ($initialResultType) {
+                    'object' => new stdClass(),
+                    'list' => [],
+                };
+            } elseif ($initialResultType !== $currentResultType) {
+                throw new LogicException('Intersection cannot be performed with different result data types such as list and object (associative array).');
+            }
+
+            if ($initialResultType === 'object') {
                 foreach ((array)$objectResult as $objectResultKey => $objectResultValue) {
                     $result->{$objectResultKey} = $objectResultValue;
                 }
+            } else {
+                $result = [...$result, ...$objectResult];
             }
         }
 
